815800a0db22a59edcfd9d451906a0e4367043dd
stas224 <stas224stas@gmail.com>
2023-04-11 00:00:46 +0300

    changes in the second hw after checking

--- 02/parser_json.py
+++ 02/parser_json.py
@@ -12,8 +12,8 @@ def example_function(log: dict):
 def parse_json(json_str: str,
                required_fields: list[str] = None,
                keywords: list[str] = None, *,
-               keyword_callback: Callable) -> bool:
-    if not all((json_str, required_fields, keywords)):
+               keyword_callback: Callable = None) -> bool:
+    if not all((json_str, required_fields, keywords, keyword_callback)):
         return False
 
     json_doc = loads(json_str)
@@ -21,6 +21,7 @@ def parse_json(json_str: str,
     for field in required_fields:
         if field in json_doc:
             for keyword in keywords:
-                if keyword in json_doc[field]:
-                    keyword_callback(field, keyword)
+                for word in json_doc[field].split():
+                    if word == keyword:
+                        keyword_callback(field, keyword)
     return True
--- 02/parser_json_test.py
+++ 02/parser_json_test.py
@@ -9,54 +9,113 @@ class TestParseJson(unittest.TestCase):
 
     def setUp(self):
         self.mock_func = mock.Mock(return_value=None)
-        self.json_example = '{"width": "500", "height": "500", "data":' \
-                            ' "Click Here", "size": "36", "style": "bold",' \
+        self.json_example = '{"width": "500 500", "height": "500", "data":' \
+                            ' "Click Here", "style": "bold rush",' \
                             ' "name": "text1", "hOffset": "250",' \
-                            ' "vOffset": "100", "alignment": "center"}'
+                            ' "vOffset": "100", "alignment": "center",' \
+                            ' "size" : "36 39"}'
         self.log = defaultdict(list)
         self.example_func = example_function(self.log)
 
     def test_parse_json_with_mock_function(self):
         res = parse_json(self.json_example,
-                         ['width', 'height'], ['500', 'Click', 'center'],
-                         keyword_callback=self.mock_func)
-        self.assertEqual(res, True)
-
-        res = parse_json(self.json_example,
                          keyword_callback=self.mock_func)
         self.assertEqual(res, False)
+        self.assertEqual([], self.mock_func.mock_calls)
 
         res = parse_json(self.json_example,
                          ['width', 'height'],
                          keyword_callback=self.mock_func)
         self.assertEqual(res, False)
+        self.assertEqual([], self.mock_func.mock_calls)
 
         res = parse_json(self.json_example,
                          keywords=['500', 'Click', 'center'],
                          keyword_callback=self.mock_func)
         self.assertEqual(res, False)
+        self.assertEqual([], self.mock_func.mock_calls)
+
+        res = parse_json(self.json_example,
+                         ['width', 'height'],
+                         ['500', 'Click', 'center'],
+                         keyword_callback=self.mock_func)
+        self.assertEqual(res, True)
+
+        expected_calls = [mock.call('width', '500'),
+                          mock.call('width', '500'),
+                          mock.call('height', '500')]
+
+        self.assertEqual(expected_calls, self.mock_func.mock_calls)
 
     def test_parse_json_with_example_function(self):
         res = parse_json(self.json_example,
-                         ['width', 'height'], ['500', 'Click', 'center'],
-                         keyword_callback=self.example_func)
-        self.assertEqual(res, True)
-
-        res = parse_json(self.json_example,
                          keyword_callback=self.example_func)
         self.assertEqual(res, False)
+        self.assertEqual(self.log, {})
 
         res = parse_json(self.json_example,
                          ['width', 'height'],
                          keyword_callback=self.example_func)
         self.assertEqual(res, False)
+        self.assertEqual(self.log, {})
 
         res = parse_json(self.json_example,
                          keywords=['500', 'Click', 'center'],
                          keyword_callback=self.example_func)
         self.assertEqual(res, False)
+        self.assertEqual(self.log, {})
 
-        self.assertEqual(self.log, {'width': ['500'], 'height': ['500']})
+        res = parse_json(self.json_example,
+                         ['width', 'height'],
+                         ['500', 'Click', 'center'])
+        self.assertEqual(res, False)
+        self.assertEqual(self.log, {})
+
+        res = parse_json(self.json_example,
+                         ['width', 'height'],
+                         ['500', 'Click', 'center'],
+                         keyword_callback=self.example_func)
+        self.assertEqual(res, True)
+
+        self.assertEqual(self.log, {'width': ['500', '500'],
+                                    'height': ['500']})
+
+    def test_parse_json_with_not_found_keywords(self):
+        res = parse_json(self.json_example,
+                         ['width', 'height'],
+                         ['50000', 'Clicker',
+                          'center_ls', '50'],
+                         keyword_callback=self.mock_func)
+        self.assertEqual(res, True)
+        self.assertEqual([], self.mock_func.mock_calls)
+
+        res = parse_json(self.json_example,
+                         ['width', 'height'],
+                         ['50', 'Cli', 'cent', '5'],
+                         keyword_callback=self.mock_func)
+        self.assertEqual(res, True)
+        self.assertEqual([], self.mock_func.mock_calls)
+
+        res = parse_json(self.json_example,
+                         ['size', 'style'],
+                         ['50000', 'Clicker', 'center_ls'],
+                         keyword_callback=self.mock_func)
+        self.assertEqual(res, True)
+        self.assertEqual([], self.mock_func.mock_calls)
+
+    def test_parse_json_with_multi_req_field(self):
+        res = parse_json(self.json_example,
+                         ['size', 'style'],
+                         ['rush', 'bold', '36', '39'],
+                         keyword_callback=self.mock_func)
+        self.assertEqual(res, True)
+
+        expected_calls = [mock.call('size', '36'),
+                          mock.call('size', '39'),
+                          mock.call('style', 'rush'),
+                          mock.call('style', 'bold')]
+
+        self.assertEqual(expected_calls, self.mock_func.mock_calls)
 
 
 if __name__ == '__main__':
