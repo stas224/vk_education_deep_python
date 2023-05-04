7701a7c660084de47fb1611a1b8a34be0060c7e8
stas224 <stas224stas@gmail.com>
2023-05-04 17:55:43 +0300

    commit hw 7

---
+++ 07/fetcher.py
@@ -0,0 +1,52 @@
+import aiohttp
+import aiofiles
+import asyncio
+from bs4 import BeautifulSoup
+import sys
+
+
+async def process_url(response):
+    text = response.text()
+    soup = BeautifulSoup(text, 'html.parser')
+    count_words = len(set(soup.get_text().split()))
+    async with aiofiles.open('count_words', 'a', encoding='UTF-8') as file:
+        await file.write(f'IN {response.url} {count_words} UNIQUE WORDS \n')
+
+
+async def fetch_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            assert response.status == 200
+            await process_url(response)
+
+
+async def run_worker(queue):
+    while True:
+        url = await queue.get()
+        try:
+            await fetch_url(url)
+        finally:
+            queue.task_done()
+
+
+async def fetch_batch(args):
+    queue = asyncio.Queue()
+    workers = [asyncio.create_task(run_worker(queue)) for _ in range(int(args[0]))]
+
+    async with aiofiles.open(args[1], 'r', encoding='UTF-8') as file:
+        async for url in file:
+            await queue.put(url)
+
+    await queue.join()
+
+    for worker in workers:
+        worker.cancel()
+
+
+def fetcher():
+    console_args = sys.argv[1:]
+    asyncio.get_event_loop().run_until_complete(fetch_batch(console_args))
+
+
+if __name__ == "__main__":
+    fetcher()
---
+++ 07/fetcher_test.py
@@ -0,0 +1,29 @@
+from unittest import mock
+from unittest.mock import Mock
+from aiounittest import AsyncTestCase
+import aiofiles
+import fetcher
+
+
+class TestUrlsParser(AsyncTestCase):
+    async def test_process_url(self):
+        response = Mock()
+        response.text = Mock(return_value='<html><body><p>hello world</p></body></html>')
+        response.url = 'http://example.com'
+        await fetcher.process_url(response)
+        with open('count_words', 'r', encoding='UTF-8') as file:
+            last_line = file.readlines()[-1]
+            assert 'IN http://example.com 2 UNIQUE WORDS' in last_line
+
+    async def test_fetch_batch(self):
+        async with aiofiles.open('test_urls.txt', 'a', encoding='UTF-8') as file:
+            await file.write(f'https://en.wikipedia.org/wiki/{1} \n')
+            await file.write(f'https://en.wikipedia.org/wiki/{2} \n')
+            await file.write(f'https://en.wikipedia.org/wiki/{3} ')
+
+        await fetcher.fetch_batch((2, 'test_urls.txt'))
+            # with open('count_words', 'r', encoding='UTF-8') as f:
+            #     output = f.readlines()
+            # # self.assertEqual(output, self.expected_output_1)
+
+
