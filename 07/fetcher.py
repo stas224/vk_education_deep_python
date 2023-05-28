c2b4c677b4206f31670b2bf6648d261bc6bd749e
stas224 <stas224stas@gmail.com>
2023-05-28 21:53:56 +0300

    fix hw 7

--- 07/fetcher.py
+++ 07/fetcher.py
@@ -1,5 +1,5 @@
 import asyncio
-import sys
+import argparse
 
 import aiohttp
 import aiofiles
@@ -26,12 +26,14 @@ async def run_worker(queue):
         url = await queue.get()
         try:
             await fetch_url(url)
+        except Exception as e:
+            print(f'With {url=}: error {e=}')
         finally:
             queue.task_done()
 
 
 async def fetch_batch(args):
-    queue = asyncio.Queue()
+    queue = asyncio.Queue(maxsize=int(args[0]))
     workers = [asyncio.create_task(run_worker(queue)) for _ in range(int(args[0]))]
 
     async with aiofiles.open(args[1], 'r', encoding='UTF-8') as file:
@@ -47,9 +49,17 @@ async def fetch_batch(args):
     await asyncio.gather(*workers, return_exceptions=True)
 
 
+def get_args() -> tuple[int, str]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", type=int, default=10, help="workers count")
+    parser.add_argument("file", nargs=1)
+    console_args = parser.parse_args()
+    return console_args.c, console_args.file[0]
+
+
 def fetcher():
-    console_args = sys.argv[1:]
-    asyncio.get_event_loop().run_until_complete(fetch_batch(console_args))
+    args = get_args()
+    asyncio.get_event_loop().run_until_complete(fetch_batch(args))
 
 
 if __name__ == "__main__":
--- 07/fetcher_test.py
+++ 07/fetcher_test.py
@@ -3,13 +3,14 @@ import unittest
 from unittest import mock
 import os
 
-from aiounittest import AsyncTestCase
+import aioresponses
+import aiounittest
 
 import fetcher
 
 
-class TestFetcher(AsyncTestCase):
-    async def test_process_url(self):
+class TestFetcher(aiounittest.AsyncTestCase):
+    async def test_process_url(self):  # no network calls here
         async def text():
             return '<html><body><p>hello world</p></body></html>'
 
@@ -26,9 +27,55 @@ class TestFetcher(AsyncTestCase):
         if os.path.exists(filename):
             os.remove(filename)
 
+    async def test_fetch_url(self):  # no network calls here
+        with aioresponses.aioresponses() as mock_url:
+            mock_url.get('http://example.com',
+                         status=200,
+                         body='<html><body><p>hello world</p></body></html>')
+            await fetcher.fetch_url('http://example.com')
+
+        filename = './count_words'
+        with open(filename, 'r', encoding='UTF-8') as file:
+            last_line = file.readlines()[-1]
+            assert 'IN http://example.com 2 UNIQUE WORDS' in last_line
+
+        if os.path.exists(filename):
+            os.remove(filename)
+
+    async def test_fetch_batch(self):  # no network calls here
+        source, output = './urls.txt', './count_words'
+        test_digits = ((8, 1), (8, 4), (8, 8), (8, 10),
+                       (1, 1), (1, 4), (1, 8), (1, 10),
+                       (100, 1), (100, 4), (100, 8), (100, 100))
+
+        for count_url, count_worker in test_digits:
+            open(output, 'w').close()
+            set_url = set()
+            with open(source, 'w', encoding='UTF-8') as file:
+                for i in range(count_url):
+                    file.write(f'http://example.com/{i}\n')
+                    set_url.add(f'IN http://example.com/{i} 2 UNIQUE WORDS \n')
+
+            with aioresponses.aioresponses() as mock_url:
+                for i in range(count_url):
+                    mock_url.get(f'http://example.com/{i}',
+                                 status=200,
+                                 body=f'<html><body><p>hello world</p></body></html>')
+                await fetcher.fetch_batch((count_worker, source))
+
+            with open(output, 'r', encoding='UTF-8') as file:
+                for line in file:
+                    self.assertIn(line, set_url)
+
+        if os.path.exists(source):
+            os.remove(source)
+
+        if os.path.exists(output):
+            os.remove(output)
+
 
 class TestFetcher2(unittest.TestCase):
-    def test_fetch_batch(self):
+    def test_fetch_batch_with_mock_fetch_url(self):  # no network calls here
         filename = './urls.txt'
         with open(filename, 'w', encoding='UTF-8') as file:
             file.write('https://en.wikipedia.org/wiki/1\n'
@@ -37,9 +84,14 @@ class TestFetcher2(unittest.TestCase):
                        'https://en.wikipedia.org/wiki/4\n')
 
         with mock.patch('fetcher.fetch_url') as mock_file:
-            asyncio.get_event_loop().run_until_complete(fetcher.fetch_batch((2, 'urls.txt')))
+            asyncio.get_event_loop().run_until_complete(fetcher.fetch_batch((4, 'urls.txt')))
 
         self.assertEqual(mock_file.call_count, 4)
 
         if os.path.exists(filename):
             os.remove(filename)
+
+
+if __name__ == '__main__':
+    aiounittest.main()
+    unittest.main()
